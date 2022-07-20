9dc85dec2df2bbd53a1ac408e743b2f63d7365be
eunji25 <jdmswl0525@naver.com>
2022-07-20 11:40:22 +0900

    html 추가

--- stock/__init__.py
+++ stock/__init__.py
@@ -6,4 +6,7 @@ def create_app():
     from.views import main_views
     app.register_blueprint(main_views.bp)
 
+    from .views import stock_views
+    app.register_blueprint(stock_views.bp)
+
     return app
